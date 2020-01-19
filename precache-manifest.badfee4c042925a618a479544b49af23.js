6356350bbbf269444339e8b398458fc8424e67f9
Roopak Chugh <chughroopak@gmail.com>
2020-01-19 14:11:22 +0530

    Updates

---
+++ precache-manifest.badfee4c042925a618a479544b49af23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cca6a8979e7d85f8d2c2d83a772f9cf",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "805331df2eba5a0fee91",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4932862f6aba8d72eee1",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "805331df2eba5a0fee91",
+    "url": "/contactmanager/static/js/2.f529174f.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.f529174f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4932862f6aba8d72eee1",
+    "url": "/contactmanager/static/js/main.398dbd4e.chunk.js"
+  },
+  {
+    "revision": "d86057960233a7428d99",
+    "url": "/contactmanager/static/js/runtime-main.2ce66dc9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactJSPractice/contactmanager/precache-manifest.cab1dd9ccb356d720044635ad5f9647c.js"
+  "/contactmanager/precache-manifest.badfee4c042925a618a479544b49af23.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactJSPractice/contactmanager/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contactmanager/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
