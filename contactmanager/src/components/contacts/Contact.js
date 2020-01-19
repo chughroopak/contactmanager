f215b183ea06b5429881b3736c1f9e1a3104ef61
Roopak Chugh <chughroopak@gmail.com>
2020-01-19 13:59:09 +0530

    final commit contactmanager

--- contactmanager/src/components/contacts/Contact.js
+++ contactmanager/src/components/contacts/Contact.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Consumer } from "../../context";
+import Axios from "axios";
+import { Link } from "react-router-dom";
 
 class Contact extends Component {
   state = {
     showContactInfo: false
   };
 
-  onDeleteClick = (id, dispatch) => {
+  onDeleteClick = async (id, dispatch) => {
+    await Axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
     dispatch({
       type: "DELETE_CONTACT",
       payload: id
@@ -37,6 +40,17 @@ class Contact extends Component {
                   className='fas fa-times'
                   style={{ cursor: "pointer", float: "right", color: "red" }}
                   onClick={this.onDeleteClick.bind(this, id, dispatch)}></i>
+
+                <Link to={`/contact/edit/${id}`}>
+                  <i
+                    className='fas fa-pencil-alt'
+                    style={{
+                      cursor: "pointer",
+                      float: "right",
+                      color: "black",
+                      marginRight: "1rem"
+                    }}></i>
+                </Link>
               </h4>
               {showContactInfo ? (
                 <ul className='list-group'>
--- contactmanager/src/context.js
+++ contactmanager/src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Axios from "axios";
 
 const Context = React.createContext();
 
@@ -16,6 +17,15 @@ const reducer = (state, action) => {
         ...state,
         contacts: [action.payload, ...state.contacts]
       };
+    case "UPDATE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.map(contact =>
+          contact.id === action.payload.id
+            ? (contact = action.payload)
+            : contact
+        )
+      };
     default:
       return state;
   }
@@ -24,29 +34,36 @@ const reducer = (state, action) => {
 export class Provider extends Component {
   state = {
     contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "555-555-5555"
-      },
-      {
-        id: 2,
-        name: "Karen Williams",
-        email: "karen@gmail.com",
-        phone: "222-222-2222"
-      },
-      {
-        id: 3,
-        name: "Henry Johnson",
-        email: "henry@gmail.com",
-        phone: "333-333-3333"
-      }
+      // {
+      //   id: 1,
+      //   name: "John Doe",
+      //   email: "jdoe@gmail.com",
+      //   phone: "555-555-5555"
+      // },
+      // {
+      //   id: 2,
+      //   name: "Karen Williams",
+      //   email: "karen@gmail.com",
+      //   phone: "222-222-2222"
+      // },
+      // {
+      //   id: 3,
+      //   name: "Henry Johnson",
+      //   email: "henry@gmail.com",
+      //   phone: "333-333-3333"
+      // }
     ],
     dispatch: action => {
       this.setState(state => reducer(state, action));
     }
   };
+
+  async componentDidMount() {
+    const res = await Axios.get("https://jsonplaceholder.typicode.com/users");
+    this.setState({
+      contacts: res.data
+    });
+  }
 
   render() {
     return (
