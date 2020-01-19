9f2a51e8622dad6d69ba7f657e8f0f61aee0fb76
Roopak Chugh <chughroopak@gmail.com>
2020-01-19 08:25:44 +0530

    My first React App

---
+++ contactmanager/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Contacts from "./components/Contacts";
+import Header from "./components/Header";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "./context";
+
+function App() {
+  return (
+    <Provider>
+      <div className='App'>
+        <Header branding='Contact Manager' />
+        <div className='container'>
+          <Contacts />
+        </div>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
